3d0b349f624075c5aa678ba418ad1a5b1759a69f
שמחה בן-דוד <sibendav@g.jct.ac.il>
2021-10-07 12:00:38 +0300

    hi this is some start - HebrewEnglishArab

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ from facebook_scraper import get_posts
 #import locale
 #print(locale.getlocale())
 
-for post in get_posts('biuconfessions2018'):
-    print(post['text'][:50])
+#Hebrew languge
+allDataHebrew = ""
+for post in get_posts('algoritmim', pages=100):
+    allDataHebrew += (post['text'] + '\n')
+print(allDataHebrew)
+
+#Enlish languge
+allDataEnglish = ""
+for post in get_posts('beaverconfessions', pages=100):
+    allDataEnglish += (post['text'] + '\n')
+print(allDataEnglish)
+
+#Arabic languge
+allDataArabic = ""
+for post in get_posts('ArabsForZehut', pages=100):
+    allDataArabic += (post['text'] + '\n')
+print(allDataArabic)
+
+
+
 
 
